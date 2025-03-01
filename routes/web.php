52cfec7e68585ed4914efc1499d4c66fd8b886dd
Kelwinkxps13 <kelwinjhackson13@gmail.com>
2025-03-01 10:55:44 -0300

    adicionada a base de cada rota que deve ser refeita

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ThemeController;
+use App\Http\Controllers\ItemController;
+use App\Http\Controllers\ContentController;
 use App\Http\Controllers\IndexController;
 use Illuminate\Support\Facades\Route;
 
@@ -32,10 +35,29 @@ Route::post('/update', [IndexController::class, 'update']);
 Route::post('/editor', [IndexController::class, 'editor']);
 
 // rotas de theme
-
+Route::get('/theme/create', [ThemeController::class, 'create']);
 Route::post('/theme/store', [ThemeController::class, 'store']);
+Route::get('/theme/show/{id}', [ThemeController::class, 'show']);
+Route::get('/theme/edit', [ThemeController::class, 'edit']);
 Route::post('/theme/update', [ThemeController::class, 'store']);
 Route::post('/theme/destroy{id}', [ThemeController::class, 'destroy']);
+
+//rotas do registro de theme (os itens)
+Route::get('/theme/{id}', [ItemController::class, 'index']);
+Route::get('/theme/{id}/create', [ItemController::class, 'create']);
+Route::post('/theme/{id}/store', [ItemController::class, 'store']);
+Route::get('/theme/{id}/{id_item}', [ItemController::class, 'show']);
+Route::get('/theme/{id}/edit/{id_item}', [ItemController::class, 'edit']);
+Route::post('/theme/{id}/update', [ItemController::class, 'update']);
+Route::post('/theme/{id}/destroy/{id_item}', [ItemController::class, 'destroy']);
+Route::get('/theme/{id}/{id_item}/editor', [ItemController::class, 'editor']);
+
+//rotas dos blocos (conteudo) de cada item
+Route::get('/theme/{id}/createblock/{id_item}', [ContentController::class, 'create']);
+Route::post('/theme/{id}/storeblock', [ContentController::class, 'store']);
+Route::get('/theme/{id}/editblock/{id_item}/{id_block}', [ContentController::class, 'edit']);
+Route::post('/theme/{id}/updateblock', [ContentController::class, 'update']);
+Route::post('/theme/{id}/destroyblock/{id_item}/{idblock}', [ContentController::class, 'destroy']);
 
 
 require __DIR__.'/auth.php';
