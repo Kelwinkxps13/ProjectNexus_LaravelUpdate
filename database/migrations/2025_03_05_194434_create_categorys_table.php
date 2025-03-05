e8901e3ae5bd44830b54d663378e26f28944cc22
Kelwinkxps13 <kelwinjhackson13@gmail.com>
2025-03-05 16:52:57 -0300

    adicionada a tabela categorys

---
+++ database/migrations/2025_03_05_194434_create_categorys_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('categorys', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->string('image')->nullable();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');  // Chave estrangeira
+            $table->timestamps();
+            $table->softDeletes();  // Soft delete
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('categorys');
+    }
+};
