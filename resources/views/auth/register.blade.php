9f60974a0ff8cef1b836fc00c97b7ae51754b4fb
Kelwinkxps13 <kelwinjhackson13@gmail.com>
2025-03-06 17:30:34 -0300

    adicionado o campo nickname quando o usuario for criar uma conta

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -16,6 +16,12 @@
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
+        <div class="mt-4">
+            <x-input-label for="nickname" :value="__('Nickname')" />
+            <x-text-input id="nickname" class="block mt-1 w-full" name="nickname" :value="old('nickname')" required autocomplete="username" />
+            <x-input-error :messages="$errors->get('nickname')" class="mt-2" />
+        </div>
+
         <!-- Password -->
         <div class="mt-4">
             <x-input-label for="password" :value="__('Password')" />
