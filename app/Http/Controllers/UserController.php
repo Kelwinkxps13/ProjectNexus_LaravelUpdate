a306f48d35171c6efb6166dded22fce43daa0657
Kelwinkxps13 <kelwinjhackson13@gmail.com>
2025-03-06 17:08:40 -0300

    aqui que vai ficar a tela inicial criada por cada usuario

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    function index () {
+        $db_main = (object) [
+            "id"=> 1,
+            "title"=> "Ola mundo",
+            "subtitle"=> "seja bem vindo a minha pagina",
+            "description"=> "aqui vou postar algumas coisas que acho interessante"
+          ]; // ainda sem contato com o banco de dados!
+
+        // variavel caso o tanto de temas deletados seja igual o tanto de temas totais
+        $final_verification = true; 
+
+        // foreach de todos os temas!
+        $themes_foreach = false;
+
+
+        return view('main', [
+            'db_main' => $db_main,
+            'final_verification' => $final_verification,
+            'themes_foreach' => $themes_foreach
+        ]);
+    }
+
+    function create () {
+        return view('indexcreator', [
+
+        ]);
+    }
+
+    function store () {
+        return redirect('/');
+    }
+
+    function edit () {
+        // pega os dados da tela inicial
+        $main = false;
+        return view('indexeditor', [
+            'main' => $main
+        ]);
+    }
+
+    function update () {
+        return redirect('/');
+    }
+
+    function editor () {
+        // pega todas as categorys registradas
+        $themes_foreach = false;
+        return view('editor', [
+            'themes_foreach' => $themes_foreach
+        ]);
+    }
+}
