e87f727300708ab1f705858ff50e21bc9a7e2eed
Kelwinkxps13 <kelwinjhackson13@gmail.com>
2025-03-07 17:14:08 -0300

    adicionada logica da funcao index no IndexController

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -3,38 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Category;
 
 class IndexController extends Controller
 {
     public function index() {
-        $db_main = (object) [
-            "id"=> 1,
-            "title"=> "Ola mundo",
-            "subtitle"=> "seja bem vindo a minha pagina",
-            "description"=> "aqui vou postar algumas coisas que acho interessante"
-          ]; // ainda sem contato com o banco de dados!
-
-        // variavel caso o tanto de temas deletados seja igual o tanto de temas totais
-        $final_verification = true; 
 
         // foreach de todos os temas!
-        $themes_foreach = [
-            (object)[
-                'id' => 1,
-                'name' => 'teste',
-                'description' => 'teste1',
-                'image' => null
-            ],
-            (object)[
-                'id' => 2,
-                'name' => 'teste',
-                'description' => 'teste2',
-                'image' => null
-            ]
-        ];
+
+        $themes_foreach = Category::orderBy('created_at', 'desc')->limit(5)->get();
+            
 
         return view('index', [
-            'final_verification' => $final_verification,
             'themes_foreach' => $themes_foreach
         ]);
     }
