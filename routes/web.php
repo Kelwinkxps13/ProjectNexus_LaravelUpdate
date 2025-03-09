11b152b00768351e45c83fb16d888da5c8c34923
Kelwinkxps13 <kelwinjhackson13@gmail.com>
2025-03-09 12:38:11 -0300

    modificados os nomes das rotas no web.php

--- routes/web.php
+++ routes/web.php
@@ -30,32 +30,32 @@ Route::middleware('auth')->group(function () {
 Route::get('/', [IndexController::class, 'index']); // feito com sucesso!
 // rotas do user
 Route::get('/{nickname}', [UserController::class, 'index']); // feito com sucesso!
-Route::get('/{nickname}/create', [UserController::class, 'create'])->middleware('iscreator'); // falta a logica do controller
-Route::post('/{nickname}', [UserController::class, 'store'])->middleware('iscreator'); // falta a logica do controller
+Route::get('/{nickname}/create', [UserController::class, 'create'])->middleware('iscreator'); // feito com sucesso!
+Route::post('/{nickname}', [UserController::class, 'store'])->middleware('iscreator'); // feito com sucesso!
 Route::get('/{nickname}/edit', [UserController::class, 'edit'])->middleware('iscreator'); // falta a logica do controller
-Route::post('/{nickname}/update', [UserController::class, 'update'])->middleware('iscreator'); // falta a logica do controller
+Route::patch('/{nickname}', [UserController::class, 'update'])->middleware('iscreator'); // falta a logica do controller
 Route::post('/{nickname}/editor', [UserController::class, 'editor'])->middleware('iscreator'); // falta a logica do controller
 
 // rotas de theme
-Route::get('/{nickname}/theme/create', [ThemeController::class, 'create'])->middleware('iscreator'); // falta a logica do controller
-Route::post('/{nickname}/theme/store', [ThemeController::class, 'store'])->middleware('iscreator'); // falta tudo
-Route::get('/{nickname}/theme/show/{id}', [ThemeController::class, 'show']); // falta a logica do controller
-Route::get('/{nickname}/theme/edit', [ThemeController::class, 'edit'])->middleware('iscreator'); // falta a logica do controller
-Route::post('/{nickname}/theme/update', [ThemeController::class, 'update'])->middleware('iscreator'); // falta tudo
-Route::post('/{nickname}/theme/destroy{id}', [ThemeController::class, 'destroy'])->middleware('iscreator'); // falta tudo
+Route::get('/{nickname}/{category}', [ThemeController::class, 'index']); // falta a logica do controller
+Route::get('/{nickname}/category/create', [ThemeController::class, 'create'])->middleware('iscreator'); // falta a logica do controller
+Route::post('/{nickname}/category', [ThemeController::class, 'store'])->middleware('iscreator'); // falta tudo
+Route::get('/{nickname}/{category}/edit', [ThemeController::class, 'edit'])->middleware('iscreator'); // falta a logica do controller
+Route::patch('/{nickname}/category', [ThemeController::class, 'update'])->middleware('iscreator'); // falta tudo
+Route::delete('/{nickname}/category', [ThemeController::class, 'destroy'])->middleware('iscreator'); // falta tudo
 
 //rotas do registro de theme (os itens)
-Route::get('/{nickname}/theme/{id}/create', [ItemController::class, 'create'])->middleware('iscreator'); // falta a logica do controller
-Route::post('/{nickname}/theme/{id}/store', [ItemController::class, 'store'])->middleware('iscreator'); // falta tudo
-Route::get('/{nickname}/theme/{id}/{id_item}', [ItemController::class, 'show']); // falta a logica do controller
-Route::get('/{nickname}/theme/{id}/edit/{id_item}', [ItemController::class, 'edit'])->middleware('iscreator'); // falta a logica do controller
-Route::post('/{nickname}/theme/{id}/update', [ItemController::class, 'update'])->middleware('iscreator'); // falta tudo
-Route::post('/{nickname}/theme/{id}/destroy/{id_item}', [ItemController::class, 'destroy'])->middleware('iscreator'); // falta tudo
-Route::get('/{nickname}/theme/{id}/show/{id_item}/editor', [ItemController::class, 'editor'])->middleware('iscreator'); // falta a logica do controller
+Route::get('/{nickname}/{category}/{id_item}', [ItemController::class, 'index']); // falta a logica do controller
+Route::get('/{nickname}/{category}/create', [ItemController::class, 'create'])->middleware('iscreator'); // falta a logica do controller
+Route::post('/{nickname}/{category}/{id_item}', [ItemController::class, 'store'])->middleware('iscreator'); // falta tudo
+Route::get('/{nickname}/{category}/edit/{id_item}', [ItemController::class, 'edit'])->middleware('iscreator'); // falta a logica do controller
+Route::patch('/{nickname}/{category}/{id_item}', [ItemController::class, 'update'])->middleware('iscreator'); // falta tudo
+Route::delete('/{nickname}/{category}/{id_item}', [ItemController::class, 'destroy'])->middleware('iscreator'); // falta tudo
+Route::get('/{nickname}/{category}/{id_item}/editor', [ItemController::class, 'editor'])->middleware('iscreator'); // falta a logica do controller
 
 //rotas dos blocos (conteudo) de cada item
-Route::get('/{nickname}/theme/{id}/createblock/{id_item}', [ContentController::class, 'create'])->middleware('iscreator'); // falta a logica do controller
-Route::post('/{nickname}/theme/{id}/storeblock', [ContentController::class, 'store'])->middleware('iscreator'); // falta tudo
-Route::get('/{nickname}/theme/{id}/editblock/{id_item}/{id_block}', [ContentController::class, 'edit'])->middleware('iscreator'); // falta a logica do controller
-Route::post('/{nickname}/theme/{id}/updateblock', [ContentController::class, 'update'])->middleware('iscreator'); // falta tudo
-Route::post('/{nickname}/theme/{id}/destroyblock/{id_item}/{idblock}', [ContentController::class, 'destroy'])->middleware('iscreator'); // falta tudo
+Route::get('/{nickname}/{category}/createblock/{id_item}', [ContentController::class, 'create'])->middleware('iscreator'); // falta a logica do controller
+Route::post('/{nickname}/{category}/storeblock', [ContentController::class, 'store'])->middleware('iscreator'); // falta tudo
+Route::get('/{nickname}/{category}/editblock/{id_item}/{id_block}', [ContentController::class, 'edit'])->middleware('iscreator'); // falta a logica do controller
+Route::patch('/{nickname}/{category}/updateblock', [ContentController::class, 'update'])->middleware('iscreator'); // falta tudo
+Route::delete('/{nickname}/{category}/destroyblock/{id_item}/{idblock}', [ContentController::class, 'destroy'])->middleware('iscreator'); // falta tudo
