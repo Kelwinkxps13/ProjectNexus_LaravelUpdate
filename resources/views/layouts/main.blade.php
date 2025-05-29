1956e5b6eef6f36655733c30e235d67ff7b98c94
Kelwinkxps13 <kelwinjhackson13@gmail.com>
2025-05-29 19:28:06 -0300

    mudada a ordem de link's com os css's. CSS do projeto foi pra ultimo

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -15,14 +15,14 @@ $data = session('user_data');
 
     {{-- - CSS do font awesome - --}}
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
-    {{-- - CSS do Projeto- --}}
-    <link rel="stylesheet" href="/css/style.css">
+
     {{-- - CSS do Google - --}}
     <link href="https://fonts.googleapis.com/css2?family=Roboto" rel="stylesheet">
     {{-- - CSS do Bootstrap - --}}
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-
+    {{-- - CSS do Projeto- --}}
+    <link rel="stylesheet" href="/css/style.css">
 
 
 </head>
