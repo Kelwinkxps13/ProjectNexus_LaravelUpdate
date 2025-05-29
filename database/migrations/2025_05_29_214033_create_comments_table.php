961b86ebf3a8adbd95b0aa81ee63ce1bd3538e61
Kelwinkxps13 <kelwinjhackson13@gmail.com>
2025-05-29 19:20:22 -0300

    parte do banco de dados do sistema de feedbacks por comentários feita

---
+++ database/migrations/2025_05_29_214033_create_comments_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->text('text')->nullable();
+            $table->foreignId('id_commenter')->constrained('users')->onDelete('cascade');
+            $table->foreignId('id_item_creator')->constrained('users')->onDelete('cascade');
+            $table->integer('comment_level')->nullable();
+            $table->json('likes')->nullable();
+            $table->json('dislikes')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('comments');
+    }
+};
