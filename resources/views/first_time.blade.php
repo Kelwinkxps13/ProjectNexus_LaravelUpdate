86246d9858155abb19a745dc2a4c5afd83580019
Kelwinkxps13 <kelwinjhackson13@gmail.com>
2025-07-18 12:06:19 -0300

    feito o sistema de tutorial de um usuário autenticado

--- resources/views/first_time.blade.php
+++ resources/views/first_time.blade.php
@@ -13,7 +13,7 @@
             <h5 class="card-title"> Editor </h5>
             <div class="card-text">
                 <p>
-                     Nessa parte, você tem pode fazer algumas coisas interessantes: 
+                    Nessa parte, você tem pode fazer algumas coisas interessantes:
                 </p>
                 <p>
                     1 - Editar sua página inicial;
@@ -21,7 +21,7 @@
                 <p>
                     2 - Editar ou excluir um tema que você criou.
                 </p>
-                
+
             </div>
             <div class="d-grid">
                 <a href="{{ $route }}" class="btn btn-primary">Continuar</a>
@@ -37,9 +37,24 @@
             <h5 class="card-title"> Temas </h5>
             <div class="card-text">
                 <p>
-                     Editor 
+                    Aqui você poder todos os itens criados dentro do tema escolhido (se tiver algum item)
                 </p>
-                
+                <p>
+                    A estrutura de compatilhamento funciona da seguinte forma: Você tem o Tema, dentro de cada tema, temos os itens, e dentro de cada item, temos o conteúdo de cada item.
+                </p>
+                <p>
+                    Exemplo:
+                    <li>
+                        Tema escolhido: Jogos
+                    </li>
+                    <li>
+                        Item do tema: Super Mario
+                    </li>
+                    <li>
+                        Dentro desse item, você tem conteúdos relacionados ao Jogo Super mario.
+                    </li>
+                </p>
+
             </div>
             <div class="d-grid">
                 <a href="{{ $route }}" class="btn btn-primary">Continuar</a>
@@ -55,9 +70,25 @@
             <h5 class="card-title"> Página Inicial </h5>
             <div class="card-text">
                 <p>
-                     Editor 
+                    Bem vindo(a) ao site Nexus!
                 </p>
-                
+                <p>
+                    Na primeira vez que você entra em cada página, você verá um tutorial como essse, que ensina como funciona o site, além de apresentar as funcionalidades.
+                </p>
+                <h5> Sobre a Página Inicial</h5>
+                <p>
+                    Na página inicial, assim que entrar pela primeira vez, você verá algumas recomendações de alguns Temas criados pela comunidade, para você dar uma olhada.
+                </p>
+                <p>
+                    Você também verá uma parte que mostra as Páginas de Boas Vindas dos usuários em si, para descobrir mais daquele criador!
+                </p>
+                <p>
+                    Crie, explore, compartilhe com outros usuários, e divirta-se!
+                </p>
+                <p>
+
+                </p>
+
             </div>
             <div class="d-grid">
                 <a href="{{ $route }}" class="btn btn-primary">Continuar</a>
@@ -73,9 +104,11 @@
             <h5 class="card-title"> Página de Boas Vindas </h5>
             <div class="card-text">
                 <p>
-                     Editor 
+                    Aqui, você da início à sua jornada de criador no site!
                 </p>
-                
+                <p>
+                    Crie sua página inicial, para prosseguir na criação dos seus próprios temas!
+                </p>
             </div>
             <div class="d-grid">
                 <a href="{{ $route }}" class="btn btn-primary">Continuar</a>
@@ -91,9 +124,9 @@
             <h5 class="card-title"> Editar Página de Boas vindas </h5>
             <div class="card-text">
                 <p>
-                     Editor 
+                    Caso tenha se arrependido de algo, ou deseja fazer alguma mudança, você pode editar sua página de boas vindas do jeito que quiser.
                 </p>
-                
+
             </div>
             <div class="d-grid">
                 <a href="{{ $route }}" class="btn btn-primary">Continuar</a>
@@ -109,9 +142,15 @@
             <h5 class="card-title"> Página de Boas Vindas </h5>
             <div class="card-text">
                 <p>
-                     Editor 
+                    Aqui você verá informaçoes como a quantidade de Temas criados pelo usuário, Quantos seguidores ele tem, e quantos usuários ele segue.
                 </p>
-                
+                <p>
+                    Essa página mostra todos os Temas de cada usuário para você navegar por eles. Quem sabe o que você descobre de novo?
+                </p>
+                <p>
+                    Além disso, você pode seguir o usuário se quiser, para acompanhar suas futuras atualizações, sejam Temas novos, Itens novos, ou até mesmo, conteúdos novos de um item.
+                </p>
+
             </div>
             <div class="d-grid">
                 <a href="{{ $route }}" class="btn btn-primary">Continuar</a>
@@ -127,9 +166,15 @@
             <h5 class="card-title"> Ver Conteúdo de um Item </h5>
             <div class="card-text">
                 <p>
-                     Editor 
+                    Essa página mostra o conteúdo de cada item que o usuário quis compartilhar com todos!
                 </p>
-                
+                <p>
+                    Exemplo: O item Harry Potter (de um tema Filmes) pode apresentar um conteúdo como: Introdução, Sobre a História, Curiosidades, O que achei do filme?, etc.
+                </p>
+                <p>
+                    O usuário é livre pra adicionar o conteúdo que ele quiser dentro de um item.
+                </p>
+
             </div>
             <div class="d-grid">
                 <a href="{{ $route }}" class="btn btn-primary">Continuar</a>
@@ -142,12 +187,15 @@
 <div class="container my-5 d-flex justify-content-center">
     <div class="card shadow-lg" style="max-width: 500px;">
         <div class="card-body">
-            <h5 class="card-title"> Editar Conteúdo de um Item </h5>
+            <h5 class="card-title"> Editar Conteúdo(s) de um Item </h5>
             <div class="card-text">
                 <p>
-                     Editor 
+                    Aqui você tem acesso à página de edição de todos os conteúdos de um item que você adicionou.
                 </p>
-                
+                <p>
+                    Basta escolher o Bloco de Conteúdo que deseja editar ou excluir, e continuar navegando.
+                </p>
+
             </div>
             <div class="d-grid">
                 <a href="{{ $route }}" class="btn btn-primary">Continuar</a>
@@ -163,9 +211,15 @@
             <h5 class="card-title"> Criar Item </h5>
             <div class="card-text">
                 <p>
-                     Editor 
+                    Após você criar seu primeiro tema, nada melhor que adicionar o primeiro item daquele tema!
                 </p>
-                
+                <p>
+                    Você pode adicionar um título, uma descrição breve do que será abordado, e uma imagem para atrair o público.
+                </p>
+                <p>
+                    Boa sorte na criação do item!
+                </p>
+
             </div>
             <div class="d-grid">
                 <a href="{{ $route }}" class="btn btn-primary">Continuar</a>
@@ -181,9 +235,9 @@
             <h5 class="card-title"> Editar Item </h5>
             <div class="card-text">
                 <p>
-                     Editor 
+                    Caso tenha se arrependido de algo, ou deseja fazer alguma mudança, você pode editar seu item do jeito que quiser.
                 </p>
-                
+
             </div>
             <div class="d-grid">
                 <a href="{{ $route }}" class="btn btn-primary">Continuar</a>
@@ -199,9 +253,15 @@
             <h5 class="card-title"> Criar um bloco de conteúdo </h5>
             <div class="card-text">
                 <p>
-                     Editor 
+                    Não é interessante deixar um item vazio após sua criação. E é exatamente por esse motivo, que você adiciona os Blocos de Conteúdo para compartilhar suas ideias daquele item.
                 </p>
-                
+                <p>
+                    A criação do bloco de conteúdo consiste basicamente em: Título, texto e uma imagem (opcional).
+                </p>
+                <p>
+                    Você pode adicionar quantos Blocos você quiser em cada item! Divirta-se na criação!
+                </p>
+
             </div>
             <div class="d-grid">
                 <a href="{{ $route }}" class="btn btn-primary">Continuar</a>
@@ -217,9 +277,9 @@
             <h5 class="card-title"> Editar um bloco de conteúdio </h5>
             <div class="card-text">
                 <p>
-                     Editor 
+                    Caso tenha se arrependido de algo, ou deseja fazer alguma mudança, você pode editar seu bloco de conteúdo do jeito que quiser do jeito que quiser.
                 </p>
-                
+
             </div>
             <div class="d-grid">
                 <a href="{{ $route }}" class="btn btn-primary">Continuar</a>
@@ -235,9 +295,27 @@
             <h5 class="card-title"> Criar tema </h5>
             <div class="card-text">
                 <p>
-                     Editor 
+                    Para dar início ao compartilhamento das coisas de seu interesse, você deve primeiro, criar um Tema a ser abordado.
                 </p>
-                
+                <p>
+                    A estrutura de compatilhamento funciona da seguinte forma: Você tem o Tema, dentro de cada tema temos os itens, e dentro de cada item, temos o conteúdo de cada item.
+                </p>
+                <p>
+                    Exemplo:
+                    <li>
+                        Tema: Jogos
+                    </li>
+                    <li>
+                        Item do tema: Super Mario
+                    </li>
+                    <li>
+                        Dentro desse item, você tem conteúdos relacionados ao Jogo Super mario.
+                    </li>
+                </p>
+                <p>
+                    Divirta-se no compartilhamento!
+                </p>
+
             </div>
             <div class="d-grid">
                 <a href="{{ $route }}" class="btn btn-primary">Continuar</a>
@@ -253,9 +331,9 @@
             <h5 class="card-title"> Editar tema </h5>
             <div class="card-text">
                 <p>
-                     Editor 
+                    Caso tenha se arrependido de algo, ou deseja fazer alguma mudança, você pode editar seu tema do jeito que quiser.
                 </p>
-                
+
             </div>
             <div class="d-grid">
                 <a href="{{ $route }}" class="btn btn-primary">Continuar</a>
